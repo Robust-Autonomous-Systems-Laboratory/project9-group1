83074c7ab76de6a038b8a6723e99cee3253c1966
Ian Q. Mattson <iqmattso@mtu.edu>
2026-04-10 22:26:00 -0400

    multiple issues, mostly from robot/computer connections and lost messages. I believe the route is overly complicated with multiple waypoints, and I believe that is causing most of our issues.  Debugging is ongoing and will continue Saturday morning.

--- planner_controller_testing/planner_controller_testing/run_combinations.py
+++ planner_controller_testing/planner_controller_testing/run_combinations.py
@@ -7,29 +7,10 @@ from ament_index_python.packages import get_package_share_directory
 import yaml
 import os
 
-lastPose = None
-
-def amcl_cb(msg):
-    lastPose = msg
-
-def drift(amcl : PoseWithCovarianceStamped, waypoint : PoseStamped):
-    if lastPose is None:
-        return -1.0, -1.0
-    x_drift = abs(amcl.pose.pose.position.x - waypoint.pose.position.x)
-    y_drift = abs(amcl.pose.pose.position.y - waypoint.pose.position.y)
-    return x_drift, y_drift
-
 def main()->None:
 
     rclpy.init()
     navigator = BasicNavigator()
-
-    amcl_sub = navigator.create_subscription(
-            PoseWithCovarianceStamped,
-            '/amcl_pose',
-            amcl_cb,
-            10)
-
 
     # load waypoint file directory from setup file
     pkg_share = get_package_share_directory('planner_controller_testing')
@@ -52,45 +33,36 @@ def main()->None:
         pose.pose.orientation.z = points[pt]['orientation'][0]
         pose.pose.orientation.w = points[pt]['orientation'][3]
         waypoints.append(pose)
-
-    #print(waypoints)
-    
-    # planner and controller combinations
-    planner_combos = ['Dijkstra', 'AStar', 'AStar']
-    controller_combos = ['RPP', 'RPP', 'DWB']
-    num_combinations = len(planner_combos)
     
     # Wait for the navigation stack to be ready
     navigator.get_logger().info("Waiting for Nav2 stack...")
     navigator.waitUntilNav2Active()
-    
-    
-    # cycle through all combinations
-    for i in range(num_combinations):
 
-        # navigator.goToPose does not support planner and controller parameters
-        # Instead, getPath()/getPathThroughPoses() must be called first, followed by followPath(), as both support changing algorithms
-        # See API here: https://docs.ros.org/en/iron/p/nav2_simple_commander/
-        path = navigator.getPathThroughPoses(waypoints[0], waypoints[1:len(waypoints)-1], planner_id=planner_combos[i])
-        navigator.followPath(path, controller_id=controller_combos[i])
-        
-        while not navigator.isTaskComplete():
-            feedback = navigator.getFeedback()
-            navigator.get_logger().info("Navigating to waypoint {feedback.current_waypoint}")
+    # begin navigating path combinations!
+    navigator.get_logger().info("Starting first combination: Dijkstra and RPP")
+    # navigator.goToPose does not support planner and controller parameters
+    # Instead, getPath()/getPathThroughPoses() must be called first, followed by followPath(), as both support changing algorithms
+    # See API here: https://docs.nav2.org/commander_api/index.html
+    path = navigator.getPathThroughPoses(waypoints[0], waypoints, planner_id='Dijkstra')
+    navigator.followPath(path, controller_id='RPP')
             
-        result = navigator.getResult()
-        if result == TaskResult.SUCCEEDED:
-            navigator.get_logger().info(f'Goal succeeded using {planner_combos[i]} planner and {controller_combos[i]} controller')
-            # Calculate drift
-            x, y = drift(lastPose, waypoints[4])
-            navigator.get_logger().info(f"Dift accumulated x: {x}, y: {y}")
-        elif result == TaskResult.CANCELED:
-            navigator.get_logger().info('Goal was canceled!')
-        elif result == TaskResult.FAILED:
-            (error_code, error_msg) = navigator.getTaskError()
-            navigator.get_logger().error('Goal failed!{error_code}:{error_msg}')
-        else:
-            navigator.get_logger().error('Goal has an invalid return status!')
-        
+    while not navigator.isTaskComplete():
+        pass
+    
+    navigator.get_logger().info("Starting second combination: A* and RPP")
+    path = navigator.getPathThroughPoses(waypoints[0], waypoints, planner_id='AStar')
+    navigator.followPath(path, controller_id='RPP')
+            
+    while not navigator.isTaskComplete():
+        pass
+    
+    navigator.get_logger().info("Starting third combination: A* and DWB")
+    path = navigator.getPathThroughPoses(waypoints[0], waypoints, planner_id='AStar')
+    navigator.followPath(path, controller_id='DWB')
+            
+    while not navigator.isTaskComplete():
+        pass
+   
+    navigator.get_logger().info("All combinations ended, Node Terminating...")
 
     navigator.lifecycleShutdown()
